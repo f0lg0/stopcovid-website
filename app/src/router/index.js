9ef383af9fab95cea3009cfb988032030726ea5c
f0lg0 <folgonileonardo@gmail.com>
2021-01-26 22:08:54 +0100

    new: handling 404s

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -11,10 +12,9 @@ const routes = [
         component: Home,
     },
     {
-        path: "/about",
-        name: "About",
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/About.vue"),
+        path: "*",
+        name: "404",
+        component: NotFound,
     },
 ];
 
---
+++ app/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="not-found" class="abs-center">
+        <h1>404</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
