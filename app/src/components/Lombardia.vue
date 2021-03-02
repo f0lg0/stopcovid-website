ec979ced1eccd22ab7d3438e57f55e3c84ea4ecf
f0lg0 <folgonileonardo@gmail.com>
2021-03-02 20:29:12 +0100

    new: same thing but for Lombardia

--- app/src/components/Lombardia.vue
+++ app/src/components/Lombardia.vue
@@ -129,54 +129,35 @@ export default {
                     break;
                 }
                 case "Variazione percentuale positivi": {
-                    let pos_per_day = [];
                     let tmp_buf = [];
-
-                    for (let i = 0; i < this.sample_reversed.length; i++) {
-                        if (
-                            (i == 0 || i % 7 == 0) &&
-                            i + 6 < this.sample_reversed.length
-                        ) {
-                            final.labels.push(
-                                `${this.sample_reversed[i].data.substring(
-                                    8,
-                                    10
-                                )}/${this.sample_reversed[i].data.substring(
-                                    5,
-                                    7
-                                )}-${this.sample_reversed[i + 6].data.substring(
-                                    8,
-                                    10
-                                )}/${this.sample_reversed[i + 6].data.substring(
-                                    5,
-                                    7
-                                )}`
-                            );
-                        }
-
-                        pos_per_day.push(
-                            this.sample_reversed[i].nuovi_positivi
-                        );
-                    }
-
-                    final.labels.shift();
-
                     let tmp = 0;
-                    for (let i = 0; i < pos_per_day.length; i++) {
-                        if (i != 0 && i % 7 == 0) {
-                            tmp_buf.push(Math.round(tmp / 7));
-                            tmp = 0;
-                        }
-                        tmp += pos_per_day[i];
-                    }
 
-                    tmp_buf.push(Math.round(tmp / 7));
+                    for (let i = 0; i < 92; i++) {
+                        for (let j = 0; j < 7; j++) {
+                            if (j == 6) {
+                                final.labels.push(
+                                    this.sample_reversed[i + j].data.substring(
+                                        5,
+                                        10
+                                    )
+                                );
+                            }
+                            tmp += this.sample_reversed[i + j].nuovi_positivi;
+                        }
+                        tmp_buf.push(Math.round(tmp / 7));
+                        tmp = 0;
+                    }
 
                     for (let i = 1; i < tmp_buf.length; i++) {
+                        console.log(i, tmp_buf[i], i - 1, tmp_buf[i - 1]);
                         final.datasets[0].data.push(
-                            this.calculatePosPerc(tmp_buf[i], tmp_buf[i - 1])
+                            this.calculatePosPerc(tmp_buf[i], tmp_buf[i - 7])
                         );
                     }
+
+                    // reducing the amt of data displayed
+                    final.labels.splice(0, 60);
+                    final.datasets[0].data.splice(0, 60);
 
                     final.datasets[0].borderColor = "#4cb5ff";
                     final.datasets[0].pointBackgroundColor = "#4cb5ff";
